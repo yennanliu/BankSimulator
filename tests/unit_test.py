9eb9c2ec595d85524890909508763b7b0f57a55f
yennanliu <f339339@gmail.com>
2019-10-10 20:06:09 +0800

    remove no need print

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -46,7 +46,6 @@ def test_account_creation():
 def test_transaction_authorization():
     bank = BankTransaction()
     expected_result = bank.transaction_authorization(amount=100, time="2019-02-13T10:00:00.000Z")
-    print ('expected_result : ', expected_result)
     assert expected_result == {"account": {"active_card": False,"account_initialized": False, "available_limit": None }, "violations": ["account_not_initialized"]}
 
 if __name__ == '__main__':
