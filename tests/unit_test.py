5e060c3b99cb4eb83c00300c190a1afa6c1d3086
yennanliu <f339339@gmail.com>
2019-10-11 16:59:15 +0800

    add tests comment

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -5,7 +5,7 @@ from bank.Bank import BankTransaction
 
 def test_read_file_input():
     """
-    test Json2NestedJson.read_file_input method 
+    test BankTransaction.read_file_input method 
     """
     expected_output = [
        {
@@ -34,16 +34,25 @@ def test_read_file_input():
     assert json_file == expected_output  
 
 def test_print_status():
+    """
+    test BankTransaction._print_status method 
+    """
     bank = BankTransaction()
     expected_result = bank._print_status() 
     assert expected_result == {"account": {"active_card": False, "account_initialized": False,"available_limit": None }, "violations": [None]}
 
 def test_account_creation():
+    """
+    test BankTransaction.account_creation method 
+    """
     bank = BankTransaction()
     expected_result = bank.account_creation(available_limit=100)
     assert expected_result == {"account": {"active_card": True,"account_initialized": True, "available_limit": 100 }, "violations": [None]}
 
 def test_transaction_authorization():
+    """
+    test BankTransaction.transaction_authorization method 
+    """
     bank = BankTransaction()
     expected_result = bank.transaction_authorization(merchant ='my_merchant', amount=100, time="2019-02-13T10:00:00.000Z")
     assert expected_result == {"account": {"active_card": False,"account_initialized": False, "available_limit": None }, "violations": ["card_not_active"]}
